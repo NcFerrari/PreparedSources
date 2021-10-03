7f076b52d16f3e6fdcbadcaf7bd202840cc3ff3d
NcFerrari <lubor.pesek@post.cz>
2021-10-03 15:38:45 +0200

    Started manuals and templates

---
+++ pom jar/src/main/java/Test.java
@@ -0,0 +1,12 @@
+import javax.swing.*;
+
+public class Test {
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame();
+        frame.setSize(500, 500);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
+    }
+}
